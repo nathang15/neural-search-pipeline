29287a520c54178699e33322cfde4511ce43ce1c
Nathan Nguyen <nhanngyn15@gmail.com>
2024-11-28 11:00:34 -0500

    reranker abc

--- rerank/base.py
+++ rerank/base.py
@@ -1,10 +1,11 @@
 import abc
 from ..pipeline import Pipeline, And, Or
 import os
-import pickle
 import typing
 import numpy as np
 import numpy.typing as npt
+from utils.batch import iterate_in_batches
+import tqdm
 
 __all__ = ["Reranker"]
 
@@ -61,103 +62,193 @@ class EmbeddingCache:
 
     def contains(self, key: str) -> bool:
         return key in self.embeddings
+
 class Reranker(abc.ABC):
     """Abstract class for ranking models."""
 
-    def __init__(self, attr: str, encoder, k: int, path: str, similarity) -> None:
-        self.attr = attr
+    def __init__(
+            self, 
+            key: str,
+            attr: typing.Union[str, typing.List[str]], 
+            encoder, 
+            normalize: bool,
+            batch_size: int,
+            k: typing.Optional[int] = None
+        ) -> None:
+        self.key = key,
+        self.attr = attr if isinstance(attr, list) else [attr]
         self.encoder = encoder
+        self.store = EmbeddingCache(key=self.key)
+        self.normalize = normalize
         self.k = k
-        self.path = path
-        self.similarity = similarity
-        self.embeddings = self.load_embeddings(path = path) if self.path else {}
+        self.batch_size = batch_size
+
+    def __len__(self) -> int:
+        return len(self.store)
 
     def __repr__(self) -> str:
-        repr_str = f"{self.__class__.__name__} Ranker"
+        repr_str = f"{self.__class__.__name__} Reranker"
+        repr_str += f"\n  Key: {self.key}"
         repr_str += f"\n  Attributes: {self.attr}"
-        repr_str += f"\n  Top-k: {self.k}"
-        repr_str += f"\n  Similarity: {self.similarity.__name__}"
-        if self.path:
-            repr_str += f"\n  Embeddings Path: {self.path}"
+        repr_str += f"\n  Normalize: {self.normalize}"
+        repr_str += f"\n  Embeddings: {len(self.store)}"
         return repr_str
 
     @abc.abstractmethod
-    def __call__(self, documents: list[dict], k: int, attr: str) -> list:
+    def __call__(
+        self,
+        q: typing.Union[typing.List[str], str], 
+        documents: typing.Union[
+            typing.List[typing.List[typing.Dict[str, str]]],
+            typing.List[typing.Dict[str, str]],
+        ],
+        k: int,
+        batch_size: typing.Optional[int] = None,
+        **kwargs,) -> typing.Union[typing.List[typing.List[typing.Dict[str, str]]], typing.List[typing.Dict[str, str]]]:
         """
-        Abstract method to rank documents.
-        
-        Args:
-            documents (list[dict]): documents input
-            k (int): k top results
-            attr (str): atrributes to rank on
-
-        Returns:
-            list: A list of top k documents
+        rerank documents based on query
         """
-        if not documents:
+        if isinstance(q, str):
             return []
-        return self
+        elif isinstance(q, list):
+            return [[]]
     
-    def add(self, documents) -> None:
+    def _encoder(self, documents: typing.List[typing.Dict[str, str]]) -> np.ndarray:
+        """computes embeddings"""
+        return self.encoder(
+            [
+                " ".join([doc.get(field, "") for field in self.attr]) for doc in documents
+            ]
+        )
+    
+    def _encode(
+        self,
+        documents: typing.List[typing.List[typing.Dict[str, str]]],
+        batch_size: typing.Optional[int] = None,
+    ) -> typing.Dict[str, np.ndarray]:
+        """Computes documents embeddings if not yet done"""
+        final, embeddings, missed = self.store.get(documents=documents)
+        if missed:
+            missed_embeddings = self._batch_encode(
+                documents=missed,
+                batch_size=batch_size,
+                desc=f"{self.__class__.__name__} Indexing missed documents",
+            )
+
+            # Merge
+            final += [document[self.key] for document in missed]
+            embeddings = embeddings + missed_embeddings
+
+        return {key: embedding for key, embedding in zip(final, embeddings)}
+    
+    def _batch_encode(self, documents: typing.List[typing.Dict[str, str]], batch_size: int, desc: str) -> typing.List[np.ndarray]:
+        """computes embeddings in batches"""
+        embeddings = []
+        for batch in iterate_in_batches(
+            sequence=documents,
+            batch_size=batch_size,
+            desc=f"{self.__class__.__name__} Reranker",
+        ):
+            embeddings.extend(self._encoder(documents = batch))
+        return embeddings
+    
+    def add(self, documents: typing.List[typing.Dict[str, str]], batch_size: int = 64) -> "Reranker":
         """
         Pre-compute embeddings and store them at the path
-
-        Args:
-            documents (list[dict]): documents input
         """
-        documents = [document[self.attr] for document in documents if document[self.attr] not in self.embeddings]
-
-        if documents:
-            for doc, emb in zip(documents, self.encoder.encode(documents)):
-                self.embeddings[doc] = emb
-            
-            if self.path:
-                self.dump_embeddings(embeddings = self.embeddings, path = self.path)
-        
+        self.store.add(
+            documents = documents,
+            embeddings=self._batch_encode(
+                documents=documents,
+                batch_size=batch_size,
+                desc=f"{self.__class__.__name__} Indexing",
+            ),
+        )
         return self
 
-    def _rank(self, similarities: list, documents: list):
+    def rank(
+        self,
+        embeddings_documents: typing.Dict[str, np.ndarray],
+        embeddings_queries: np.ndarray,
+        documents: typing.List[typing.List[typing.Dict[str, str]]],
+        k: int,
+        batch_size: typing.Optional[int] = None,
+    ) -> list: 
         """
-        Rank documents based on similarities.
-
-        Args:
-            similarities (list): list of similarities
-            documents (list): list of documents
-
-        Returns:
-            list: A list of ranked documents
+        Rank documents based on similarities among returned top k
         """
-        top_similarities = sorted(similarities, key=lambda x: -x[1])[:self.k]
-        return [
-            {**documents[idx], "similarity": score} for idx, score in top_similarities
-        ]
+        # Reshape if needed
+        if len(embeddings_queries.shape) == 1:
+            embeddings_queries = embeddings_queries.reshape(1, -1)
 
-    @staticmethod
-    def load_embeddings(path: str):
-        """
-        Load embeddings from a file.
+        # Normalize embeddings for cosine similarity
+        if self.normalize:
+            embeddings_queries = (
+                embeddings_queries
+                / np.linalg.norm(embeddings_queries, axis=-1)[:, None]
+            )
 
-        ### Parameters
-        - **path** (`str`): Path to the embeddings file.
+        # Compute sim scores
+        scores, missing = [], []
+        for q, batch in tqdm.tqdm(
+            zip(embeddings_queries, documents), position=0, desc="Ranker scoring"
+        ):
+            if batch:
+                scores.append(
+                    q
+                    @ np.stack(
+                        [embeddings_documents[d[self.key]] for d in batch], axis=0
+                    ).T
+                )
+                missing.append(False)
+            else:
+                # Did not find any doc for the query
+                scores.append(np.array([]))
+                missing.append(True)
 
-        ### Returns
-        - (`Dict`): Loaded embeddings.
-        """
-        if not os.path.isfile(path):
-            return {}
-        with open(path, "rb") as f:
-            return pickle.load(f)
+        ranked = []
+        for scores_query, documents_query, missing_query in tqdm.tqdm(
+            zip(scores, documents, missing), position=0, desc="Reranking"
+        ):
+            if missing_query:
+                ranked.append([])
+                continue
 
-    @staticmethod
-    def dump_embeddings(embeddings, path: str) -> None:
-        """
-        ### Parameters
-        - **embeddings**: Embeddings to save.
-        - **path**: Path to save the embeddings.
-        """
-        os.makedirs(os.path.dirname(path), exist_ok=True)
-        with open(path, "wb") as f:
-            pickle.dump(embeddings, f)
+            scores_query = scores_query.reshape(1, -1)
+            ranks_query = np.fliplr(np.argsort(scores_query))
+            scores_query, ranks_query = scores_query.flatten(), ranks_query.flatten()
+            ranks_query = ranks_query[:k]
+            ranked.append(
+                [
+                    {
+                        **document,
+                        "Similarity": similarity,
+                    }
+                    for document, similarity in zip(
+                        np.take(documents_query, ranks_query),
+                        np.take(scores_query, ranks_query),
+                    )
+                ]
+            )
+
+        return ranked
+    
+    def encode_rank(
+        self,
+        embeddings_queries: np.ndarray,
+        documents: typing.List[typing.List[typing.Dict[str, str]]],
+        k: int,
+        batch_size: typing.Optional[int] = None,
+    ) -> typing.List[typing.List[typing.Dict[str, str]]]:
+        """ Encoder documents and rerank based on the query """
+        embeddings_documents = self._encode(documents=documents, batch_size=batch_size)
+        return self.rank(
+            embeddings_documents=embeddings_documents,
+            embeddings_queries=embeddings_queries,
+            documents=documents,
+            k=k,
+            batch_size=batch_size,
+        )
 
     def __add__(self, other):
         """Pipeline operator."""
